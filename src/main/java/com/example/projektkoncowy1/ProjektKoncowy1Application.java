7e1d26b24b591aa27473deeedbc857570ffc3cf7
Grzegorz Domanowski <grzegorz.domanowski.ais@gmail.com>
2022-05-18 20:40:47 +0200

    init

---
+++ src/main/java/com/example/projektkoncowy1/ProjektKoncowy1Application.java
@@ -0,0 +1,13 @@
+package com.example.projektkoncowy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektKoncowy1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektKoncowy1Application.class, args);
+    }
+
+}
